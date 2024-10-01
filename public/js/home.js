a9ff0c1328ff2e2323dde848246d9f877ce822c3
Banothu Srinivas <banavathsrinvas405@gmail.com>
2024-10-01 09:56:33 +0530

    Refactor home page layout and styles

---
+++ public/js/home.js
@@ -0,0 +1,13 @@
+let cust = document.querySelector("#customer");
+let retai = document.querySelector("retailer");
+let person = "customer";
+cust.addEventListener("click", () => {
+  if (person === "shipper") {
+  }
+});
+
+retai.addEventListener("click", () => {
+  if (person === "customer") {
+    let heading;
+  }
+});
