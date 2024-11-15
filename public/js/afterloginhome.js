2febd6cc59d80108099b11377e1a22a5f6786a36
Padda Rohit Kumar <rohitkumarsoftware@gmail.com>
2024-11-15 16:12:53 +0530

    Enhance after-login home UI with new styles and improved event handling for mode toggle

--- public/js/afterloginhome.js
+++ public/js/afterloginhome.js
@@ -3,17 +3,27 @@ let mode = document.querySelector("#newmodebtn");
 mode.addEventListener("click", () => {
     if (click == 0) {
         click = 1;
-        document.querySelector("#profile").style.display = "block";
+      document.querySelector("#newdiv").style.display = "block";
+      document.querySelector("#imgprofile").src = "circle-x.svg";
     } else {
         click = 0;
-        document.querySelector("#profile").style.display = "none";
+        document.querySelector("#newdiv").style.display = "none";
+        document.querySelector("#imgprofile").src = "user - Copy.svg";
+      mode.style.backgroundColor = "white";
+       mode.addEventListener("mouseout", () => {
+         ele.style.backgroundColor = "#f3f4f6";
+       });
+      mode.addEventListener("mouseout", () => {
+        ele.style.backgroundColor = "white";
+      })
     }
 })
 let state = 0;
 let mode1 = document.querySelector("#modebtn");
 mode1.addEventListener("click", () => {
   if (state == 0) {
-       event.preventDefault();
+    event.preventDefault();
+    document.querySelector("#modebtn1").src = "sun-moon.svg";
         state = 1;
         document.querySelector("body").style.backgroundColor = "black";
         document.querySelector("#heading").style.color = "white";
@@ -60,11 +70,21 @@ mode1.addEventListener("click", () => {
       })
       document.querySelector("#caution").style.backgroundColor = "#3F1D0D";
     document.querySelector("#caution").style.color = "#F5D383";
-    document.querySelector("#forming").src = "camera (1).svg"; //doubt
-    
+    // document.querySelector("#forming").src = "camera (1).svg"; //doubt
+    document.querySelector("#body").style.backgroundColor = "black";
+    document.querySelectorAll(".contents").forEach((ele) => {
+      ele.style.color = "white";
+    })
+    document.querySelector("#profilephoto").style.backgroundColor = "white";
+    document.querySelector("#bodyheading").style.color = "white";
+    document.querySelectorAll(".addressheading").forEach((ele) => {
+      ele.style.color = "white";
+    });
     } else {
        event.preventDefault();
-        state = 0;
+    state = 0;
+      document.querySelector("#modebtn1").src = "moon.svg";
+     mode1.src = "moon.svg";
         document.querySelector("body").style.backgroundColor = "white";
         document.querySelector("#heading").style.color = "black";
          document.querySelectorAll(".starti").forEach((ele) => {
@@ -108,6 +128,14 @@ mode1.addEventListener("click", () => {
        });
       document.querySelector("#caution").style.backgroundColor = "#FFF9E5";
     document.querySelector("#caution").style.color = "black";
-     document.querySelector("#forming").src = "camera.svg"; //doubt
+    //  document.querySelector("#forming").src = "camera.svg"; //doubt
+    document.querySelector("#body").style.backgroundColor = "white";
+     document.querySelectorAll(".contents").forEach((ele) => {
+       ele.style.color = "black";
+     });
+    document.querySelector("#bodyheading").style.color = "black";
+     document.querySelectorAll(".addressheading").forEach((ele) => {
+       ele.style.color = "black";
+     });
     }
 })
